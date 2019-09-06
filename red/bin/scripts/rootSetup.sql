4f3d1af75fa6372fba561fad1c1ba577c4b6e03c
egalli64 <egalli64@gmail.com>
2019-09-06 14:01:20 +0200

    remove files that had not to be committed

--- red/bin/scripts/rootSetup.sql
+++
@@ -1,8 +0,0 @@
--- c:> sqlplus / as sysdba
--- sql> @rootSetup.sql
-
-create user red identified by red account unlock;
-grant connect, resource to red;
-alter user red quota unlimited on users;
-
---exit
